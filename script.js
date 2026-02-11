3294bed72ec9f3e3274af324cdba5b45652e13e0
quietwas <r3dmike@gmail.com>
2026-02-11 15:50:21 -0500

    Implemented game logic for wins and draws
    Created two new functions 'checkGameState' and 'checkAvailableTiles' in order to validate a winning board and a completed game board respectively

--- script.js
+++ script.js
@@ -65,15 +65,60 @@ function GameController() {
         console.log(`${getActivePlayer().name}'s turn.`)
     }
 
+    const checkGameState = () => {
+        const gameState = board.getBoard()
+
+        for(let i = 0; i < 3; i++){
+            if (gameState[i][0].getValue() === gameState[i][1].getValue() 
+                && gameState[i][0].getValue() === gameState[i][2].getValue() 
+                && gameState[i][0].getValue() !== 0){
+                    return true
+                }
+            if (gameState[0][i].getValue() === gameState[1][i].getValue() 
+                && gameState[0][i].getValue() === gameState[2][i].getValue() 
+                && gameState[i][0].getValue() !== 0){
+                    return true
+                }
+        }
+
+        if (gameState[0][0].getValue() === gameState[1][1].getValue() 
+            && gameState[0][0].getValue() === gameState[2][2].getValue() 
+            && gameState[0][0].getValue() !== 0){
+                return true
+            }
+        if (gameState[0][2].getValue() === gameState[1][1].getValue() 
+            && gameState[0][2].getValue() === gameState[2][0].getValue() 
+            && gameState[0][2].getValue() !== 0){
+                return true
+            }
+
+        return false
+    }
+
+    const checkAvailableTiles = () => {
+        const gameState = board.getBoard()
+
+        return gameState.some(row => row.some(cell => cell.getValue() === 0))
+    }
+
     const playRound = (row, col) => {
         if (board.getBoard()[row][col].getValue() !== 0) return
 
-        console.log(`Placing ${getActivePlayer.name}'s token into row ${row}, column ${col}...`)
+        console.log(`Placing ${getActivePlayer().name}'s token into row ${row}, column ${col}...`)
 
         board.placeToken(row, col, getActivePlayer().token)
 
-        switchPlayerTurn()
-        printNewRound()
+        if (checkGameState()){
+            return console.log(`${getActivePlayer().name} won!`)
+        }
+
+        if(checkAvailableTiles()){
+            switchPlayerTurn()
+            printNewRound()
+        } else{
+            board.printBoard()
+            return console.log("It's a draw")
+        }
     }
 
     printNewRound()
@@ -81,12 +126,19 @@ function GameController() {
     return {
         playRound,
         getActivePlayer,
+        checkGameState,
+        checkAvailableTiles,
         getBoard: board.getBoard
     }
 }
 
 const game = GameController()
+game.playRound(0, 0)
+game.playRound(1, 1)
 game.playRound(0, 1)
-game.playRound(1, 1)
-game.playRound(1, 1)
-game.playRound(1, 1)
+game.playRound(0, 2)
+game.playRound(2, 0)
+game.playRound(1, 0)
+game.playRound(1, 2)
+game.playRound(2, 1)
+game.playRound(2, 2)
